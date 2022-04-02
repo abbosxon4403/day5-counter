22c1922aef4b574aec7be701f873e4a16450d64c
abbosxon <abbosxon.oripov@yandex.ru>
2022-04-02 14:42:05 +0500

    add 100$ just in case

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 // homework -----------------------------------------------------------------------------------------------
-let som = 11111111;
+let som = 10000;
 let dollar = som / 9433;
 let evro = som / 10354;
 let ticket = 500;
 let hotel = 250;
 let museum = 120;
-console.log("ticket =", ticket, '$', "hotel =", hotel, '$', "museum =", museum, 'dollar')
+console.log("ticket =", ticket, '$', "hotel =", hotel, '$', "museum =", museum, '$')
 console.log("SO'M =", Math.round(som), "DOLLAR =", Math.round(dollar), "EVRO =", Math.round(evro));
 if (som <= 0) {
     dollar = 0;
@@ -13,8 +13,9 @@ if (som <= 0) {
     console.log("hech narsa yoq");
 }
 console.log("Qoldi =", Math.round(dollar - (ticket + hotel + museum)), "dollar")
-if (dollar - ticket + hotel + museum <= 0) {
+if (dollar - ticket + hotel + museum <= 100) {
     console.log("oka samolyotga ozgina pul yetmiyapti");
+    console.log("ozingiz uchun , har ehtimolga qarshi oldingizda 100$ dan ko'p pul tursin")
 } else {
     console.log("yaxshi yo'l sizga! ");
 }
